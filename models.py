02a1921322673d2d8caec05b2dc8a653a43792c0
andywar65 <andy.war1965@gmail.com>
2017-09-13 22:43:42 +0200

    app skeleton

---
+++ models.py
@@ -0,0 +1,13 @@
+from django import forms
+from django.db import models
+
+from modelcluster.fields import ParentalKey, ParentalManyToManyField
+from modelcluster.contrib.taggit import ClusterTaggableManager
+from taggit.models import TaggedItemBase
+
+from wagtail.wagtailcore.models import Page, Orderable
+from wagtail.wagtailcore.fields import RichTextField
+from wagtail.wagtailadmin.edit_handlers import FieldPanel, InlinePanel, MultiFieldPanel
+from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
+from wagtail.wagtailsearch import index
+from wagtail.wagtailsnippets.models import register_snippet
