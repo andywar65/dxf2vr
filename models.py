7a6ac5da59476a9d5bc683bd91c2bddc2d7c6803
andywar65 <andy.war1965@gmail.com>
2017-09-14 00:37:20 +0200

    Adding fields to model

--- models.py
+++ models.py
@@ -1,13 +1,56 @@
 from django import forms
 from django.db import models
 
-from modelcluster.fields import ParentalKey, ParentalManyToManyField
+from modelcluster.fields import ParentalKey
 from modelcluster.contrib.taggit import ClusterTaggableManager
 from taggit.models import TaggedItemBase
 
 from wagtail.wagtailcore.models import Page, Orderable
-from wagtail.wagtailcore.fields import RichTextField
-from wagtail.wagtailadmin.edit_handlers import FieldPanel, InlinePanel, MultiFieldPanel
+from wagtail.wagtailadmin.edit_handlers import FieldPanel, InlinePanel
 from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
 from wagtail.wagtailsearch import index
-from wagtail.wagtailsnippets.models import register_snippet
+from wagtail.wagtaildocs.models import Document
+from wagtail.wagtaildocs.edit_handlers import DocumentChooserPanel
+
+class Dxf2VrPage(Page):
+    intro = models.CharField(max_length=250, null=True, blank=True,)
+    equirectangular_image = models.ForeignKey(
+        'wagtailimages.Image', 
+        null=True,
+        blank=True,
+        on_delete = models.SET_NULL, 
+        related_name = '+',
+        )
+    dxf_file = models.ForeignKey(
+        'wagtaildocs.Document', 
+        null=True, 
+        on_delete = models.SET_NULL,
+        related_name = '+',
+        )
+
+    search_fields = Page.search_fields + [
+        index.SearchField('intro'),
+        #index.SearchField('body'),
+    ]
+
+    content_panels = Page.content_panels + [
+        FieldPanel('intro'),
+        DocumentChooserPanel('dxf_file'),
+        ImageChooserPanel('equirectangular_image'),
+        InlinePanel('material_images', label="Material Image Gallery",),
+    ]
+
+class Dxf2VrPageGalleryImage(Orderable):
+    page = ParentalKey(Dxf2VrPage, related_name='material_images')
+    image = models.ForeignKey(
+        'wagtailimages.Image', on_delete=models.CASCADE, related_name='+', 
+        null=True, blank=True,
+    )
+    layer = models.CharField(max_length=250,)
+    color = models.CharField(max_length=250, null=True, blank=True,)
+
+    panels = [
+        FieldPanel('layer'),
+        ImageChooserPanel('image'),
+        FieldPanel('color'),
+    ]
